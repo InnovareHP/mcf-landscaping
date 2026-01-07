ab621c3120a6e28f0a38441dc07e9c0e6d07fcd2
Abc <cresencioabcdef26@gmail.com>
2026-01-07 21:43:04 +0800

    feat (main): fixed main

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import Footer from "@/components/Footer";
 import Work from "./work/page";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-white">
-      {/* Main Content */}
-      <main className="w-full py-16">
-        <div className="mx-[8%]">
-          <Work />
-        </div>
-      </main>
-
-      <Footer />
-    </div>
-  );
+  return <Work />;
 }
